2f7b2fee0b0ca38001e409c78a68ed6f4a9321bb
Linlin Li <paris1900lll@gmail.com>
2021-05-12 00:18:10 +0100

    fghj

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 // write your code here
 
-function createpetcard(pet){
+function createpetcard(dataid,j){
 let sectionel = document.querySelector('.image-container')
 let articleel = document.createElement('article')
 articleel.setAttribute('class','image-card' )
 let titleel = document.createElement('h2')
 titleel.setAttribute('class','title')
-titleel.innerText = pet.title
+titleel.innerText = dataid.title
 let imgel = document.createElement('img')
 imgel.setAttribute('class','image')
 //variable
-imgel.setAttribute('src', pet.image)
+imgel.setAttribute('src', dataid.image)
 let divlikes = document.createElement('div')
 divlikes.setAttribute('class','likes-section')
 let spanel = document.createElement('span')
 spanel.setAttribute('class','likes')
-spanel.innerText =`${pet.likes} likes`
+spanel.innerText =`${dataid.likes} likes`
 let likebtn = document.createElement('button')
 likebtn.setAttribute('class','like-button')
 likebtn.innerText = '♥' 
-likebtn.addEventListener('click', function (event){
-    event.stopPropagation()
-    let newpet = pet.likes + 1
-    updatelikes(newpet)
+let newpet = dataid.likes
+likebtn.addEventListener('click', function (){
+    newpet++
+    console.log(newpet)
+    updatelikes(newpet, spanel,j)
 })
-
 
 divlikes.append(spanel,likebtn)
 let ulel = document.createElement('ul')
 ulel.setAttribute('class','comments')
 
-for(const comment of pet.comments){
+for(const comment of dataid.comments){
     let liel = document.createElement('li')
     liel.innerText = comment.content
     ulel.append(liel)
@@ -48,18 +48,44 @@ postel.classList.add('comment-button')
 postel.setAttribute('type','submit')
 postel.innerText = 'post'
 formel.append(inputel,postel)
-formel.addEventListener('submit',function (event){
-    event.preventDefault()
-})
+addcomment(formel,ulel,j)
 articleel.append(titleel,imgel,divlikes, ulel,formel )
 sectionel.append(articleel)
 
 }
+function addcomment(formel,ulel,j){
+    formel.addEventListener('submit',function (event){
+        event.preventDefault()
+        let liel = document.createElement('li')
+        liel.innerText = formel.comment.value
+        ulel.prepend(liel) 
+        fetch(`http://localhost:3000/comments`,{
+            method:'POST',
+            headers: {'Content-Type':"application/json"},
+            body: JSON.stringify({
+                content: `${formel.comment.value}`,
+                imageId: `${j}` 
+            })
+        })
+        .then(function(response){
+            console.log(response)
+            return response.json()
+        })
+        .then(function(response){
+            liel.innerText = response.content
+            ulel.prepend(liel) 
+        })
+        // formel.reset()
+    })
+}
+
 
 
 function createcards(data){
-    for(const element of data ){
-        createpetcard(element)
+    for(let i = 0; i< data.length; i++ ){
+        let dataid = data[i]
+        let j= i+1
+        createpetcard(dataid,j)
     }
 }
 
@@ -74,12 +100,18 @@ fetch("http://localhost:3000/images")
 )
 
 
-
-function updatelikes(response){
-    fetch('http://localhost:3000/images/1', {
+function updatelikes(newlikes, spanel,j){
+    console.log(newlikes)
+    fetch(`http://localhost:3000/images/${j}`, {
     method:'PATCH',
-    headers: {'Content-Type':"appliation/json"},
-    body: JSON.stringify({likes:response})
-    } 
-    )
+    headers: {'Content-Type':"application/json"},
+    body: JSON.stringify({likes:newlikes})
+    })
+    .then(function(response){
+        return response.json()
+    }
+    ).then(function(response){
+        spanel.innerText = `${response.likes} likes`
+    })
 }
+
